24fb5f63c19888a34db6a1ce88266a6c5bbddd2a
EugenioSC <147703762+EugenioSC@users.noreply.github.com>
2025-03-04 22:52:31 +0000

    Rotamiento

--- sketch.js
+++ sketch.js
@@ -1,14 +1,34 @@
-function preload() {
-  // put preload code here
-}
+let grado = Math.PI/180
+let r = grado
 
 function setup() {
   // put setup code here
   createCanvas(800,800)
+  rectMode(CENTER)
+  background(255)
+  noCursor()
+}
+
+function dibujo(x,y,grados){
+  push()
+  translate(x,y)
+  rotate(grados)
+  rect(0,0,100,100)
+  pop()
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+textSize(42)
+for(let i=0; i<20; i++){
+  let x = random(0,width)
+  let y = random(0,height)
+  let rojo = random(0,255)
+  let verde = random(0,255)
+  let azul = random(0,255)
+  fill(rojo,verde,azul,80)
+  dibujo(x,y,r)
+  r += grado
+  }
+  text("🧸",mouseX,mouseY)
 }
